4168809a33bf50353cb3d0b1fe6759301ee6ff7c
Vaidas <shibalba1@gmail.com>
2020-11-18 22:02:27 +0100

    update

--- src/background.js
+++ src/background.js
@@ -1,27 +1,39 @@
+const qs = require('qs');
+
 chrome.webRequest.onBeforeRequest.addListener(
     async () => {
-        const series = await new Promise(resolve => {
+        const seriesUri = await new Promise((resolve, reject) => {
             chrome.tabs.executeScript({
-                code: 'document.querySelector(\'.tv-series-episode-list-item--selected\').getAttribute(\'href\')'
-            }, results => resolve(results[0]));
+                code: 'document.querySelector(\'.tv-series-episode-list-item--selected\').getAttribute(\'href\');'
+            }, results => results !== undefined ? resolve(results[0]) : reject('Failed to fetch series URI.'));
         });
-        console.log(series)
+        const seriesSplit = seriesUri.split('/');
+        const query = {
+            title: seriesSplit[2].replace('-', ' '),
+            season: seriesSplit[4],
+            episode: seriesSplit[6],
+        };
+        // console.log('http://localhost:3000/http://localhost:3001/?' + qs.stringify(query));
 
         return {
-            redirectUrl: "http://localhost:3000/http://localhost:3001/ss"
+            redirectUrl: 'http://localhost:3000/http://localhost:3001/?' + qs.stringify(query),
         }
     },
-    {urls: ["*://undertekst.nrk.no/*"]},
-    ["blocking"]
+    {
+        urls: ['*://undertekst.nrk.no/*']
+    },
+    ['blocking']
 );
 
 chrome.webRequest.onBeforeSendHeaders.addListener(
-    details => ({
-        requestHeaders: {
-            ...details.requestHeaders,
-            'X-Requested-With': 'https://tv.nrk.no',
-        }
-    }),
-    {urls: ["*://undertekst.nrk.no/*"]},
-    ["blocking"]
+    details => {
+        details.requestHeaders['X-Requested-With'] = 'https://tv.nrk.no';
+        return {
+            requestHeaders: details.requestHeaders
+        };
+    },
+    {
+        urls: ['*://undertekst.nrk.no/*']
+    },
+    ['blocking', 'requestHeaders', 'extraHeaders']
 );
